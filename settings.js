2fad6877a1dda88711d6ffbd02c70d877580249a
vghdse <h.hanayan66@gmail.com>
2025-09-19 19:03:59 +0200

    Update settings.js for Mine

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~929ac36c1d1a087981755ccf619795fc5bf774e6",
+  OWNER_NUMBER: "263", 
+  PREFIX: ",",
+  CDN: "https://mrfrankk-cdn.hf.space" // 
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
