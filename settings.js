279cad4fdf74ebbf9de094a2c50b4c57d6bcc6d3
vghdse <h.hanayan66@gmail.com>
2025-10-04 21:32:56 +0200

    Update settings.js for subzeronjhh

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~6587bea37138ad2747d101e713df23b7eb480753", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ",", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Vb",
+  OWNER_NUMBER: "Vv", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space" // 
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
